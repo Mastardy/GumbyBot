8cfd3ba8bf548395c705303cce2002d4b07e7ca9
ogniK <25727384+ogniK5377@users.noreply.github.com>
2023-08-21 20:29:15 +1000

    Boilerplate
    Make it all async
    Handle our .env
    Login to discord & just chill

---
+++ GumbyBot/Program.cs
@@ -0,0 +1,50 @@
+﻿using Discord.WebSocket;
+
+namespace GumbyBot
+{
+	public class Program
+	{
+		private DiscordSocketClient? _client = null;
+
+		public static Task Main(string[] args) => new Program().MainAsync(args);
+
+		public async Task MainAsync(string[] args)
+		{
+			// Load our .env file & fetch the token
+			// Gotta use TraversePath so then we can look backwards in folders too! Helps for debugging
+			DotNetEnv.Env.TraversePath().Load();
+			var token = Environment.GetEnvironmentVariable("token");
+
+			// Failed to find .env or it doesn't have a token
+			if(token == null)
+			{
+				Console.Error.WriteLine("Failed to fetch discord app token!");
+				return;
+			}
+
+			try
+			{
+				_client = new();
+
+				// When we connect to discord, just verify shit works
+				_client.Connected += Task () => {
+					Console.WriteLine($"Logged in as user {_client.CurrentUser.Username}({_client.CurrentUser.Id})");
+					return Task.CompletedTask;
+				};
+
+				// Login
+				await _client.LoginAsync(Discord.TokenType.Bot, token);
+				await _client.StartAsync();
+			}
+			catch (Exception ex) {
+				Console.Error.WriteLine("Failed to start/login to discord!");
+				Console.Error.WriteLine(ex.Message);
+				Console.Error.WriteLine(ex?.StackTrace?.ToString());
+			}
+
+			// Wait forever
+			await Task.Delay(-1);
+		}
+
+	}
+}
