689e26e81c8d1e69d897e587db01ee13a8af9544
ogniK <25727384+ogniK5377@users.noreply.github.com>
2023-08-26 21:59:55 +1000

    No need to make HttpClient in our singleton

--- GumbyBot/Services/AsyncHttpWebRequest.cs
+++ GumbyBot/Services/AsyncHttpWebRequest.cs
@@ -10,17 +10,19 @@ namespace GumbyBot.Services
 	[Service]
 	public class AsyncHttpWebRequest
 	{
-		static readonly HttpClient _client = new();
+		readonly HttpClient _client;
 
 		public AsyncHttpWebRequest()
 		{
+			_client = new();
+
 			ProductInfoHeaderValue product = new("GumbyBot", "1.0");
 			_client.DefaultRequestHeaders.UserAgent.Add(product);
 		}
 
 		public async Task<string> String(HttpMethod method, string url)
 		{ 
-			HttpRequestMessage request = new HttpRequestMessage(method, url);
+			HttpRequestMessage request = new(method, url);
 			using HttpResponseMessage response = await _client.SendAsync(request);
 			response.EnsureSuccessStatusCode();
 			return await response.Content.ReadAsStringAsync();
